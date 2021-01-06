e3d2a11edca8c6b44a067af20ab572c63f3acdf5
RyannnnC <41617365+RyannnnC@users.noreply.github.com>
2021-01-07 03:10:48 +1100

    side bar finish
    1

--- App.js
+++ App.js
@@ -7,6 +7,8 @@ import ProviderOrder from './Scene/providerOrder.js'
 import ProviderMain from './Scene/providerMain.js'
 import Account from './Scene/account.js'
 import Plan from './Scene/plan.js'
+import { MaterialCommunityIcons, MaterialIcons } from '@expo/vector-icons';
+import { AntDesign } from '@expo/vector-icons';
 
 export default function App() {
   const Tab = createBottomTabNavigator();
@@ -14,10 +16,46 @@ export default function App() {
   return (
     <NavigationContainer>
         <Tab.Navigator>
-          <Tab.Screen name="home" component={ProviderMain} />
-          <Tab.Screen name="order" component={ProviderOrder} />
-          <Tab.Screen name="plan" component={Plan} />
-          <Tab.Screen name="account" component={Account} />
+          <Tab.Screen
+            name="主页"
+            component={ProviderMain}
+            options={{
+              tabBarLabel: '主页',
+              tabBarIcon: ({ color, size }) => (
+                <MaterialIcons name="home" size={size} color={color} />
+              ),
+            }}
+            />
+          <Tab.Screen
+            name="订单"
+            component={ProviderOrder}
+            options={{
+              tabBarLabel: '订单',
+              tabBarIcon: ({ color, size }) => (
+                <MaterialCommunityIcons name="calendar-text-outline" size={size} color={color} />
+              ),
+            }}
+          />
+          <Tab.Screen
+            name="行程"
+            component={Plan}
+            options={{
+              tabBarLabel: '行程',
+              tabBarIcon: ({ color, size }) => (
+                <AntDesign name="calendar" size={size} color={color} />
+              ),
+            }}
+             />
+          <Tab.Screen
+            name="账号"
+            component={Account}
+            options={{
+              tabBarLabel: '账号',
+              tabBarIcon: ({ color, size }) => (
+              <MaterialCommunityIcons name="account" color={color} size={size} />
+            ),
+          }}
+             />
         </Tab.Navigator>
     </NavigationContainer>
   );
